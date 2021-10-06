ed097d8f8f94073e2ae535f4b0c85320f5999039
hdogrul <hdogrul@MacBook-Pro.local>
2021-10-06 05:11:53 -0400

    removing moongoose related key from server js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
@@ -17,7 +17,6 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
-    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
